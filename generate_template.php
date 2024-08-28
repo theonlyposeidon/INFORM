953d2b34cd4e9bf0a48d6d03cd1f73dd829d5af3
theonlyposeidon <theonlyposeidon@gmail.com>
2024-08-29 07:10:44 +1000

    Update generate_template.php

--- generate_template.php
+++ generate_template.php
@@ -81,7 +81,7 @@ EOT;
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   $informType = $_POST['informType'];
   $systemName = $_POST['systemName'];
-  $insertDate = date_format($_POST['insertDate'], "l, d F Y");
+  $insertDate = $_POST['insertDate']->format("l, d F Y");
   $todayDate = date("l, d F Y"); // Format: DDD, dd MMM yyyy
 
   // Generate the INFORM message based on the user's selection
