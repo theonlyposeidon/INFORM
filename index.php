7da30889f17b2f05ee4ad1eebf715c18972fc2d4
theonlyposeidon <theonlyposeidon@gmail.com>
2024-08-29 20:32:09 +1000

    update index.php with values.json

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$data = json_decode(file_get_contents('data.json'), true);
+$data = json_decode(file_get_contents('values.json'), true);
 
 $informTypes = $data['informTypes'];
 $systemNames = $data['systemNames'];
